b53f3d7179933af36816bc98e74671038bba26eb
dylan hough <dylanpjhough@gmail.com>
2024-03-04 13:51:29 +0000

    race class, fields, constructor and getters/setters

---
+++ src/cycling/Race.java
@@ -0,0 +1,29 @@
+package cycling;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Race {
+    private int ID;
+    private String name;
+    private String description;
+    private HashMap<Rider, Integer> riders;//riders -> points
+    private ArrayList<Stage> stages;
+    private double length; 
+
+    public Race(int id, String name, String description) {
+        ID = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getID(){ return ID; }
+    public String getName() { return name; }
+    public String getDescription() { return description; }
+    public double getLength() { return length; }
+
+    public void setName(String name) { this.name = name; }
+    public void setDescription(String description) { this.description = description; }
+    public void setLength(double length) { this.length = length; }
+
+
+}
