228d7b921f6bba27e4dde7bae86ebfa8889cdffc
DylanHough <60287993+Hayai1@users.noreply.github.com>
2024-03-10 11:52:30 +0000

    Revert "Remove rider"

--- src/cycling/Rider.java
+++ src/cycling/Rider.java
@@ -3,24 +3,24 @@ package cycling;
 import java.util.UUID;
 
 public class Rider {
-    private int ID;
-    private int teamID;
+    private UUID ID;
+    private UUID teamID;
     private String name;
     private int yearOfBirth;
 
-    public Rider(int id, int teamID, String name, int yearOfBirth) {
-        ID = id;
+    public Rider(UUID teamID, String name, int yearOfBirth) {
+        ID = java.util.UUID.randomUUID();
         this.teamID = teamID;
         this.name = name;
         this.yearOfBirth = yearOfBirth;
     }
 
-    public int getID(){ return ID; }
-    public int getTeamID() { return teamID; }
+    public UUID getID(){ return ID; }
+    public UUID getTeamID() { return teamID; }
     public String getName() { return name; }
     public double getYearOfBirth() { return yearOfBirth; }
 
     public void setName(String name) { this.name = name; }
-    public void setTeamID(int teamID) { this.teamID = teamID; }
+    public void setTeamID(UUID teamID) { this.teamID = teamID; }
     
 }
